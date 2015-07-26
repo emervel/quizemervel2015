5008ff4543bb7cdbaaafab4dc6ba38d3abe46df5
emervel <eduardomerchanvelasco@gmail.com>
2015-07-26 17:22:19 +0200

    Tema8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -27,14 +27,14 @@ exports.index = function(req, res) {
 	models.Quiz.findAll({where: ["pregunta like ?", search], order :"pregunta ASC"}).then(
 	//models.Quiz.findAll().then(
 		function(quizes) {
-			res.render('quizes/index',{quizes: quizes});
+			res.render('quizes/index',{quizes: quizes, errors:[]});
 		}
 	).catch(function(error){next(error);})	
 };
 
 //GET /quizes/:id
 exports.show = function(req, res) {
-	res.render('quizes/show',{quiz: req.quiz});	
+	res.render('quizes/show',{quiz: req.quiz, errors:[]});	
 };
 
 //GET /quizes/:id/answer
@@ -43,12 +43,76 @@ exports.answer = function(req, res) {
 	if(req.query.respuesta.toUpperCase() === req.quiz.respuesta.toUpperCase()) {
 		resultado = 'Correcto';
 	} 
-	res.render('quizes/answer',	{quiz : req.quiz, respuesta: resultado});		
+	res.render('quizes/answer',	{quiz : req.quiz, 
+								respuesta: resultado,
+								errors:[]});		
+};
+
+//GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build(
+		{pregunta : "Pregunta", respuesta : "Respuesta"}
+	);	 
+	res.render('quizes/new', {quiz : quiz, errors:[]});		
+};
+
+//GET /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+	quiz.validate().then(	
+		function(err) {
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+				//guarda en BBDD los campos pregunta y respuesta de quiz
+				quiz.save({fields : ["pregunta", "respuesta"]}).then(
+					function(){
+						res.redirect('/quizes')
+					}
+				)
+			}
+		}
+	);
+};
+
+//GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz;
+	res.render('quizes/edit',{quiz: quiz, errors: []});
+};
+
+//GET /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	req.quiz.validate().then(	
+		function(err) {
+			if (err) {
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			} else {
+				//guarda en BBDD los campos pregunta y respuesta de quiz
+				req.quiz.save({fields : ["pregunta", "respuesta"]}).then(
+					function(){
+						res.redirect('/quizes')
+					}
+				)
+			}
+		}
+	);
+};
+
+//DELETE /quizes/:id
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then( function() {
+			res.redirect('/quizes');
+		}		
+	).catch(function(error){next(error)});
 };
 
 //GET /quizes/authors
 exports.authors = function(req, res) {
-	res.render('quizes/authors',{autor: 'Eduardo Merchan Velasco'});
+	res.render('quizes/authors',{autor: 'Eduardo Merchan Velasco',errors: []});
 };
 
 //OLD
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors:[] });
 });
 
 router.param('quizId', quizController.load); // autoload : quizId
@@ -12,6 +12,11 @@ router.param('quizId', quizController.load); // autoload : quizId
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 /*Cambios para el tema 7
 router.get('/quizes/question', quizController.question);
